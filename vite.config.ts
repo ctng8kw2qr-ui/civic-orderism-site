f6704ab99e9f61119965ac99528b46e73d2dba65
赵鹏博 <zhaopengbo@zhaopengbodeMacBook-Air-2.local>
2026-05-09 13:59:27 -0400

    Refactor site with React and Vue

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  },
+});
