f8bfdbdd3f875974576953eddee45d8483758ec3
赵鹏博 <zhaopengbo@zhaopengbodeMacBook-Air-2.local>
2026-05-09 22:27:35 -0400

    Add low dimensional party politics article

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
         main: "index.html",
         articles: "articles.html",
         internalChangeExternalChange: "articles/internal-change-external-change.html",
+        partyPoliticsLowDimensionalFunction: "articles/party-politics-low-dimensional-function.html",
       },
     },
   },
